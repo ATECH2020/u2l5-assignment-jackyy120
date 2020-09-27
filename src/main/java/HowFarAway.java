ed90ff82cbccf16194cabe38edfe93699bf3115c
Jackelyn Guzman <jackelyn.1768257@nv.ccsd.net>
2020-09-27 19:33:25 +0000

    Created and initialized the vars for HowFarAway

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,29 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+       
+      //declare vars
+      double startLat;
+      double startLong;
+      double endLat;
+      double endLong;
+
+      Scanner scanner = new Scanner(System.in);
+      //initialize vars
+      System.out.println("Enter the latitue of the starting location: ");
+      startLat = scanner.nextDouble();
+
+      System.out.println("Enter the longitude of the startinig location: ");
+      startLong = Scanner.nextDouble();
+
+      System.out.println("Enter the latitude of the ending location: ");
+      endLat = scanner.nextDouble();
+
+      System.out.println("Enter the longitidue of the ending location: ");
+      endLong = scanner.nextDouble();
+
+
+
+      
     }
 }
