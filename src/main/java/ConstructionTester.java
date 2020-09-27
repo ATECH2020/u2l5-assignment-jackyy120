7c5814fdc747e8e6a19f095494f64e1f297f8c5c
Jackelyn Guzman <jackelyn.1768257@nv.ccsd.net>
2020-09-27 18:57:49 +0000

    I made some changes to the constructionTester, mainly with prompting for variables

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,29 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+      //declare variables
+        double taxRate; 
+        int numBoards;
+        int numWindows;
+
+        Scanner scanner = new Scanner(System.in);
+        Construction myConstruction = new Construction(8.0,11.0,taxRate);
+
+        //prompt user and initialize vars
+        System.out.println("Enter the sales tax rate: ");
+        taxRate = scanner.nextDouble();
+
+        System.out.println("How many boards do you need?");
+        numBoards = scanner.nextInt();
+
+        System.out.println("How many windows do you need?");
+        numWindows = scanner.nextInt();
+
+        //finding the total
+        double total = (numBoards * 8) + (numWindows * 11); 
+        System.out.println("Total: "+total);
+
+        //finding the grandTotal
+        System.out.print("Grand Total: ");
     }
 }
